a49039b629a624491b0c39b0a9110e6584840bec
Divyansh Jha <divyanshj.16@gmail.com>
2017-02-24 15:58:23 +0530

    complete mult.asm and add the missing Mult.hack

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@mult
+M = 0
+
+@i
+M = 1
+
+(LOOP)
+@R1
+D = M
+@i
+D = M - D
+@END
+D;JGT      //jUMP if greater than 0
+
+@R0
+D = M
+@mult
+M = M + D
+@i
+M = M + 1
+
+@mult
+D = M
+@R2
+M = D
+
+@LOOP
+0;JMP
+
+
+
+(END)
+@END
+
+0;JMP
