3a3d1b2bcf57f1c88c2c40f9cff4e30a430e856d
Divyansh Jha <divyanshj.16@gmail.com>
2017-03-18 19:28:27 +0530

    Parser for assembler written in python

---
+++ 06/HackAssembler/parser.py
@@ -0,0 +1,46 @@
+class Parser(object):
+	"""Parser for hack assembly language. It can decode the instruction into its different parts."""
+
+
+	def __init__(self, instruction):
+		self.instruction = instruction
+		self.comp = ''
+		self.dest = ''
+		self.jmp = ''
+		self.parse();
+
+	def	type(self):
+		if (self.instruction.startswith('@')):
+			return "A"
+		else:
+			return "C"
+
+	def parse(self):
+		if not (self.type() == 'A'):
+			part1 = self.instruction.split(';');
+			try:
+				self.jmp = part1[1]
+			except:
+				print("No Jmp statement")
+			finally:
+				instructionPart = part1[0]
+			part2 = instructionPart.split('=')
+			if len(part2) > 1:
+				print("intruction with destination")
+				self.dest = part2[0]
+				self.comp = part2[1]
+			else:
+				self.comp= part2[0]
+
+def main():
+	instruction = input("Enter a Instruction: ")
+	if len(instruction) < 1:
+		instruction = "dest=comp;jmp"
+	parser = Parser(instruction);
+	print("comp = " + parser.comp)
+	print("dest = " + parser.dest)
+	print("jmp = " + parser.jmp)
+	print("type = " + parser.type())
+
+if __name__ == '__main__':
+	main()
