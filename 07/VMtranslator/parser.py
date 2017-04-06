e896bb12b9a77c9f991fa8f94557651280221b08
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-06 18:26:53 +0530

    add command_type method returns the type of command

--- 07/VMtranslator/parser.py
+++ 07/VMtranslator/parser.py
@@ -1,5 +1,6 @@
 class Parser(object):
-    types_of_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
+    types_of_arithmetic_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
+
 
     def cleaner(self,command):
         command =  command.strip()
@@ -8,8 +9,14 @@ class Parser(object):
         elif command.startswith("//"):
             return ""                           #for empty line
         else:
-            return command.split('//')[0].strip()
+            return command.split('//')[0].strip()  #for inline comments and normal commands
 
 
     def command_type(self,command):
-        pass
+        com_type = command.split()[0]
+        if com_type in Parser.types_of_arithmetic_commands:
+            return 0                                # For arithmetic commands i.e c_arithmetic
+        if com_type == 'push':
+            return 1                                # For c_push or push commands
+        if com_type == 'pop':
+            return 2                                # For c_pop commands
