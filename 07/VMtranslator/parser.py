fd02e106231b36e2d49d9ed7279f4d6ee1a926ae
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-06 20:32:06 +0530

    Add split_command function in the parser and add constructor and initialised defaults value which will be changed by cleaner and type_command functions

--- 07/VMtranslator/parser.py
+++ 07/VMtranslator/parser.py
@@ -1,22 +1,42 @@
 class Parser(object):
     types_of_arithmetic_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
 
+    def __init__(self):
+        self.arg1 = ''
+        self.arg2 = ''
+        self.command = ''
+        self.type = 100
 
     def cleaner(self,command):
-        command =  command.strip()
-        if len(command) < 1:
+        stripped_command =  command.strip()
+        if len(stripped_command) < 1:
             return ""                           #for empty line
-        elif command.startswith("//"):
+        elif stripped_command.startswith("//"):
             return ""                           #for empty line
         else:
-            return command.split('//')[0].strip()  #for inline comments and normal commands
+            self.command = stripped_command.split('//')[0].strip()
+            return self.command                   #for inline comments and normal commands
 
 
     def command_type(self,command):
         com_type = command.split()[0]
         if com_type in Parser.types_of_arithmetic_commands:
+            self.type = 0
             return 0                                # For arithmetic commands i.e c_arithmetic
         if com_type == 'push':
+            self.type = 1
             return 1                                # For c_push or push commands
         if com_type == 'pop':
+            self.type = 2
             return 2                                # For c_pop commands
+
+        #for c_return split method is not called
+
+    def split_command(self,command):
+        parts = command.split()
+        if self.type == 0:
+            self.arg1 = command
+            self.arg2 = ''
+        if self.type == 1 or self.type == 2:
+            self.arg1 = parts[1]
+            self.arg2 = parts[2]
