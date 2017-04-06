41297a83a28ec7c77618a76fe824d0da062ff63c
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-06 17:55:49 +0530

    add methods to the test parser class to test the cleaner method(ALL OK)

--- 07/VMtranslator/testParser.py
+++ 07/VMtranslator/testParser.py
@@ -1,5 +1,32 @@
 import unittest
+from parser import Parser
+from unittest.mock import Mock
 
 class testParser(unittest.TestCase):
     """Test class for Parser class."""
-    pass
+
+    def setUp(self):
+        self.parser = Parser()
+
+    def test_cleaner_comments(self):
+        cleaned = self.parser.cleaner('//This is a comment')
+        self.assertEqual(cleaned,"","The line is a Comment")
+
+    def test_cleaner_whitespaces(self):
+        cleaned = self.parser.cleaner('  add   ',)
+        self.assertEqual(cleaned,"add","command stripped")
+
+    def test_cleaner_inlineComments(self):
+        self.assertEqual(self.parser.cleaner('add   //this adds 2 stack'),'add',"comment removed")
+
+    def test_cleaner_emptyline(self):
+        self.assertEqual(self.parser.cleaner('               \n'),'','empty line')
+
+    # def test_command_type_arithmetic(self):
+    #     types_of_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
+    #     assertEquals(parser.command_type(" add "),0,"Arithmetic type command")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
