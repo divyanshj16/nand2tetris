1a813722c2fcecbd5aa8e79771ec6f8287534d2f
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-06 18:28:13 +0530

    update testParser for testing command type (ALL OK)

--- 07/VMtranslator/testParser.py
+++ 07/VMtranslator/testParser.py
@@ -1,6 +1,5 @@
 import unittest
 from parser import Parser
-from unittest.mock import Mock
 
 class testParser(unittest.TestCase):
     """Test class for Parser class."""
@@ -22,9 +21,25 @@ class testParser(unittest.TestCase):
     def test_cleaner_emptyline(self):
         self.assertEqual(self.parser.cleaner('               \n'),'','empty line')
 
-    # def test_command_type_arithmetic(self):
-    #     types_of_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
-    #     assertEquals(parser.command_type(" add "),0,"Arithmetic type command")
+    def test_command_type_arithmetic(self):
+        types_of_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
+        self.assertEqual(self.parser.command_type("add"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("sub"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("neg"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("eq"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("gt"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("lt"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("and"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("or"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("or"),0,"Arithmetic type command")
+        self.assertEqual(self.parser.command_type("not"),0,"Arithmetic type command")
+
+    def test_command_type_push(self):
+        self.assertEqual(self.parser.command_type('push segment index'),1,"push type command")
+
+    def test_command_type_pop(self):
+        self.assertEqual(self.parser.command_type('pop segment index'),2,"pop type command")
+
 
 
 
