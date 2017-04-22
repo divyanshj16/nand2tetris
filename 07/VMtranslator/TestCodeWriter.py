65b881c6355fee9a198dcdade22c02bb95bf16d3
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-22 13:35:31 +0530

    add tests for gt and lt commands

--- 07/VMtranslator/TestCodeWriter.py
+++ 07/VMtranslator/TestCodeWriter.py
@@ -25,6 +25,25 @@ class TestCodeWriter(unittest.TestCase):
         converted_string = self.cdw.write_arithmetic('eq')
         self.assertEqual(converted_string,'//eq\n@SP\nAM = M - 1\nD = M\n@SP\nAM = M - 1\n@EQ_2\nD = M - D;JEQ\n@SP\nA = M\nM = 0\n@DEFAULT_2\n0;JMP\n(EQ_2)\nM = -1\n(DEFAULT_2)\n@SP\nM = M + 1\n')
 
+    def test_write_arithmetic_gt(self):
+        converted_string = self.cdw.write_arithmetic('gt')
+        self.assertEqual(converted_string,'//gt\n@SP\nAM = M - 1\nD = M\n@SP\nAM = M - 1\n@GT_1\nD = M - D;JGT\n@SP\nA = M\nM = 0\n@DEFAULT_1\n0;JMP\n(GT_1)\nM = -1\n(DEFAULT_1)\n@SP\nM = M + 1\n')
+        self.cdw.write_arithmetic('gt')
+        self.cdw.write_arithmetic('eq')
+        converted_string = self.cdw.write_arithmetic('gt')
+        self.assertEqual(converted_string,'//gt\n@SP\nAM = M - 1\nD = M\n@SP\nAM = M - 1\n@GT_3\nD = M - D;JGT\n@SP\nA = M\nM = 0\n@DEFAULT_4\n0;JMP\n(GT_3)\nM = -1\n(DEFAULT_4)\n@SP\nM = M + 1\n')
+
+    def test_write_arithmetic_lt(self):
+        converted_string = self.cdw.write_arithmetic('lt')
+        self.assertEqual(converted_string,'//lt\n@SP\nAM = M - 1\nD = M\n@SP\nAM = M - 1\n@LT_1\nD = M - D;JLT\n@SP\nA = M\nM = 0\n@DEFAULT_1\n0;JMP\n(LT_1)\nM = -1\n(DEFAULT_1)\n@SP\nM = M + 1\n')
+        self.cdw.write_arithmetic('lt')
+        self.cdw.write_arithmetic('eq')
+        self.cdw.write_arithmetic('gt')
+        converted_string = self.cdw.write_arithmetic('lt')
+        self.assertEqual(converted_string,'//lt\n@SP\nAM = M - 1\nD = M\n@SP\nAM = M - 1\n@LT_3\nD = M - D;JLT\n@SP\nA = M\nM = 0\n@DEFAULT_5\n0;JMP\n(LT_3)\nM = -1\n(DEFAULT_5)\n@SP\nM = M + 1\n')
+
+
+
 
 
 if __name__ == '__main__':
