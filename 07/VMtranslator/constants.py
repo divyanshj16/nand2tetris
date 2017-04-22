bdf750ee576f2aee1c3e790f015688d543038cee
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-22 17:48:47 +0530

    Add some more few constants

--- 07/VMtranslator/constants.py
+++ 07/VMtranslator/constants.py
@@ -1,2 +1,3 @@
 types_of_arithmetic_commands = ['add','sub','neg','eq','gt','lt','and','or','not']
 types_of_segments = ['argument','local','static','constant','this','that','pointer','temp']
+type_of_commands = ['arithmetic','push','pop']
